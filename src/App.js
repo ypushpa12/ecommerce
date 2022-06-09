079a306be9fb7b3e116f1846d647b8b21b657b76
ypushpa12 <pushpalatha.yarra@gspann.com>
2022-06-09 20:43:34 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navbar from './component/Navbar';
 import Products from './component/Products';
 import Product from './component/Product';
 import { Switch, Route } from 'react-router-dom';
+import Home from './component/Home';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
       <Switch>
         <Route exact path="/" component={Products} />
         <Route exact path="/products/:id" component={Product} />
---
+++ src/component/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="hero">
+            <div class="card bg-dark text-white">
+                <img src="/assets/shopping1.jpg" class="card-img" alt="Background image"/>
+                <div class="card-img-overlay">
+                    <h5 class="card-title display-3 fw-bolder mb-0">Welcome to Fashion World</h5>
+                    <p class="card-text"></p>
+                    <p class="card-text"></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
